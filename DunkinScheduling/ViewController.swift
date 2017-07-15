28238a6cae54c9ac2cfc9a7de17078ec17fc695c
Logan Schultz <schultzlogana@gmail.com>
2017-07-15 18:41:27 -0400

    Initial Commit

---
+++ DunkinScheduling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DunkinScheduling
+//
+//  Created by Logan Schultz on 7/15/17.
+//  Copyright © 2017 Logan Schultz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
